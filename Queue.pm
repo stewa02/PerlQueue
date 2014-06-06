40adfd6adda9fd1c851151f61f7d23d75daf5125
stewa02 <stephan1995@bluewin.ch>
2014-06-06 22:12:24 +0200

    Documentation and additional features

--- Queue.pm
+++ Queue.pm
@@ -2,6 +2,11 @@ package Queue;
 use strict;
 use warnings;
 
+# General Information:
+*CREATOR = \"Stephan Wagner";
+*VERSION = \1.0;
+*PACKAGE = \"Queue";
+
 sub new {
 	my $type = shift;
     my %params = @_;
@@ -10,6 +15,23 @@ sub new {
     $self->{'Length'} = $params{'Length'};
 	$self->{'Data'} = [];
     bless($self, $type);
+}
+
+sub pick {
+	my $self = shift;
+	my $pickitem = $self->{'Data'}[0];
+	return $pickitem;
+}
+
+sub clear {
+	my $self = shift;
+	undef($self->{'Data'});
+}
+
+sub delete {
+	my $self = shift;
+	my $delitem = shift($self->{'Data'});
+	undef($delitem);
 }
 
 sub get {
@@ -31,3 +53,50 @@ sub add {
 }
 
 1;
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+Queue - Simple object oriented queue
+
+=head1 VERSION
+
+version 1.0
+
+=head1 DESCRIPTION
+
+The Queue module is the simplest object oriented package for creating
+queues.
+
+=head1 METHODS
+
+=head2 new
+
+Creates a new instance of a queue object.
+
+=head2 add [ELEMENT]
+
+Adds the a specified element to the queue.
+
+=head2 get 
+
+Returns the first element in the queue and deletes the element from the queue.
+
+=head2 delete
+
+Removes the first element in the queue without returning it.
+
+=head2 clear
+
+Removes all elements from the queue.
+
+=head2 pick
+
+Returns the first element of the queue, without removing it.
+
+=cut
