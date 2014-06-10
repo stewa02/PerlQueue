ec052c31896cf1fbe27c7418093b57dec1115e44
stewa02 <stephan1995@bluewin.ch>
2014-06-10 08:25:22 +0200

    v1.6

--- doc/gen_pdf_doc.bat
+++ doc/gen_pdf_doc.bat
@@ -5,5 +5,5 @@ rem Version: 1.0.0
 rem Generates PDF-Documentation out of POD using pod2pdf.
 
 echo "Generate PDF..."
-pod2pdf documentation.pod >documentation.pdf
+pod2pdf --title Queue documentation.pod >documentation.pdf
 pause
